1cf109257ed01ac7fe14505792ebc84b201380f5
Quinton Alexander <60115102+Qma939@users.noreply.github.com>
2022-03-21 12:02:12 -0700

    Java SpringBoot folder replaced by "Demo"

--- Java SpringBoot/src/main/java/com/example/demo/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-    // used to create the CRUD function.
-}
