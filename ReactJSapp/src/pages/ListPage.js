15cf67b956ce2ca119ec5983bb24ad14a904d5f1
Qma939 <qma93960@ucmo.edu>
2022-03-30 14:40:52 -0500

    temp commenting out for later testing table scripting

--- ReactJSapp/src/pages/ListPage.js
+++ ReactJSapp/src/pages/ListPage.js
@@ -24,16 +24,18 @@ const headerKey = ["Project", "Requester ID",
     "Date Requested", "Date Approved"];
 
 // row -> x , col -> y
+let i, j;
 let rowPos = [];
 let colPos = [];
-let cellPos = [rowPos, colPos];  // cellPos[x, y]  ... map these together
+let cellPos = [rowPos[i], cellPos[j]];  // cellPos[rowPos[i]][colPos[j]];  ... map these together
 // need a data structure to use some temp row data for the table
 // somehow map them together based off of the header array above
 // id=col1, id=col2, col3, ..., something like this ?
 
                 // props
 const ListPage = () => {
-    let str;
+    // Build an adjustable table by using ...
+    //     cellPos[rowPos[i], colPos[j]]
     return (
         // Listing contents in table format:
         <div>
@@ -42,7 +44,15 @@ const ListPage = () => {
             <MyBody>
             <Table
                 variant="default"
-                style={{width: "80%", margin: "250px auto",
+                style={{ <thead style={{backgroundColor:"#36dc07", textAlign:"center"}}>
+                {/*---- NOTE: tr elements using the js mapping will be useful to auto fill data into our tables*/}
+                    <tr>
+                    <th width={1}>#</th>
+                {Array.from({ length: colMax }).map((_, index) => (
+                    <th key={index}>{headerKey[index]}</th>
+                    ))}
+                    </tr>
+                    </thead>width: "80%", margin: "250px auto",
                     backgroundColor:"#00fa68",
                     border: "2px solid", borderColor:"#00622c", boxShadow:"8px 3px 3px 1px #00622c",
                     textAlign: "left",
@@ -50,37 +60,33 @@ const ListPage = () => {
             striped bordered hover responsive>
             <caption>Summary of software product license management</caption>
     {/*---------- Table Header Row*/}
-                <thead style={{backgroundColor:"#36dc07", textAlign:"center"}}>
-{/*---- NOTE: tr elements using the js mapping will be useful to auto fill data into our tables*/}
-                <tr>
-                    <th width={1}>#</th>
-                    {Array.from({ length: colMax }).map((_, index) => (
-                        <th key={index}>{headerKey[index]}</th>
-                    ))}
-                </tr>
-                </thead>
-    {/*---------- End for Table Header Row*/}
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    {Array.from({ length: colMax }).map((_, index) => (
 
-                        <td key={index}>cell(1, {index})</td>
-                    ))}
-                </tr>
-                <tr>
-                    <td>2</td>
-                    {Array.from({ length: colMax }).map((_, index) => (
-                        <td key={index}>cell(2, {index})</td>
-                    ))}
-                </tr>
-                <tr>
-                    <td>3</td>
-                    {Array.from({ length: colMax }).map((_, index) => (
-                        <td key={index}>cell(3, {index})</td>
-                    ))}
-                </tr>
+                <tbody>
+                {entryArray.map(this.renderEntry)}
                 </tbody>
+    {/*---------- End for Table Header Row*/}
+    {/*            <tbody>*/}
+    {/*            <tr>*/}
+    {/*                <td>1</td>*/}
+    {/*                {Array.from({ length: colMax }).map((_, index) => (*/}
+
+    {/*                    <td key={index}>cell(1, {index})</td>*/}
+    {/*                ))}*/}
+    {/*            </tr>*/}
+    {/*            <tr>*/}
+    {/*                <td>2</td>*/}
+    {/*                {Array.from({ length: colMax }).map((_, index) => (*/}
+    {/*                    <td key={index}>cell(2, {index})</td>*/}
+    {/*                ))}*/}
+    {/*            </tr>*/}
+    {/*            <tr>*/}
+    {/*                <td>3</td>*/}
+    {/*                {Array.from({ length: colMax }).map((_, index) => (*/}
+    {/*                    <td key={index}>cell(3, {index})</td>*/}
+    {/*                ))}*/}
+    {/*            </tr>*/}
+    {/*            </tbody>*/}
+
             </Table>
             </MyBody>
             <Footer></Footer>
@@ -88,6 +94,19 @@ const ListPage = () => {
         </div>
     );
 };
+
+function renderEntry(entry, index){
+    return(
+        <tr key={index}>
+            <td>{entry.Project}</td>
+            <td>{entry.Requester}</td>
+            <td>{entry.License}</td>
+            <td>{entry.Status}</td>
+            <td>{entry.DateReq}</td>
+            <td>{entry.DateApprvd}</td>
+        </tr>
+    )
+}
 
 export default ListPage;
 
